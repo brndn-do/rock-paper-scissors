36b6946b65cfb49c42329b8c91a5c2a1916e57fa
brndn-do <dobrandon05@gmail.com>
2024-07-06 17:35:34 -0500

    Write computer choice function
    Wrote a function that returns the computer's choice (rock, paper, or
    scissors).

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 "use strict";
 
+function getComputerChoice() {
+    let rand = Math.floor(Math.random() * 3);
+    let choice = ""
+
+    if (rand === 0) {
+        choice = "rock";
+    }
+    else if (rand === 1) {
+        choice = "paper";
+    }
+    else {
+        choice = "scissors";
+    }
+
+    return choice;
+}
